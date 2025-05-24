5d482372e734f28b003c10f53fe425cf807e1a29
Artur B. <arturbobb96@gmail.com>
2025-05-24 18:10:41 +0200

    Added a centralized file for reexport to simplify usings directives

---
+++ ALog/ALog.cs
@@ -0,0 +1,19 @@
+﻿// Reexport as centalized file.
+
+// Core
+global using ALog.Core;
+
+// Config
+global using ALog.Config;
+
+// Interfaces
+global using ALog.Public.Interfaces;
+
+// Writers
+global using ALog.Writers.Console;
+global using ALog.Writers.File;
+
+// Formatters
+global using ALog.Formatters;
+
+namespace ALog;
