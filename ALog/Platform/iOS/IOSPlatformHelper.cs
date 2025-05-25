b559cc748461c6af605b5e936c5d1ee279524385
Artur B. <arturbobb96@gmail.com>
2025-05-25 16:49:28 +0200

    Implemented Platformhelpers

---
+++ ALog/Platform/iOS/IOSPlatformHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace ALog.Platform.iOS;
+
+using System;
+using System.IO;
+
+public class IOSPlatformHelper : IPlatformHelper
+{
+    public string ResolveLogFilePath(string relativePath)
+    {
+        var basePath = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+            "..", "Library", "Logs");
+
+        return Path.Combine(basePath, "ALog", relativePath);
+    }
+
+    public string GetPlatformName() => "iOS";
+}
